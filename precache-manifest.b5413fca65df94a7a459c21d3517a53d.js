5c0b235c3bed8ad0022cd9c3ede5c9626a3bd187
zuoteng <lancelotnemoj@outlook.com>
2020-06-08 19:16:49 +0800

    update

---
+++ precache-manifest.b5413fca65df94a7a459c21d3517a53d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623bf22fd71ab0b38a3fdd3781906147",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dac96ec5af8f29f0ac8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf1d1f6337e5a5bbab2d",
+    "url": "/static/js/2.402301f9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.402301f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dac96ec5af8f29f0ac8",
+    "url": "/static/js/main.97e994ba.chunk.js"
+  },
+  {
+    "revision": "433fe78bf624c6ee6ada",
+    "url": "/static/js/runtime-main.3e6cb422.js"
+  }
+]);
